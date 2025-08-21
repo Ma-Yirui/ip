ad024779aa689149aa1c8fbc8f784b26498e64a3
Ma-Yirui <saltiestwonton@gmail.com>
2025-08-22 02:42:55 +0800

    Implement basic todo list functions: add and list

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,23 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class ChatBot {
     private final IOComponent IOComponent =  new IOComponent();
     private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private final ArrayList<String> toDoList = new ArrayList<String>();
 
     public void run() throws IOException{
         this.IOComponent.greet();
         String nextLine = bufferedReader.readLine();
         while (true){
             nextLine = nextLine.trim();
-            if (nextLine.equals("bye")){
+            if (nextLine.equalsIgnoreCase("bye")){
                 this.IOComponent.exit();
                 return;
+            } else if (nextLine.equalsIgnoreCase("list")){
+                this.printList();
             } else {
-                this.IOComponent.echo(nextLine);
-                nextLine = bufferedReader.readLine();
+                this.addToList(nextLine);
             }
+            nextLine = bufferedReader.readLine();
         }
     }
+
+    public void addToList(String input){
+        this.toDoList.add(input);
+        this.IOComponent.printAddToList(input);
+    }
+
+    public void printList(){
+        this.IOComponent.printList(this.toDoList.toString()
+                .replace("[", "").replace("]", "").split(","));
+    }
+
 }
--- src/main/java/IOComponent.java
+++ src/main/java/IOComponent.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class IOComponent {
     private static final String separator = new String(new char[50]).replace('\0', '-');
@@ -21,6 +22,20 @@ public class IOComponent {
     public void echo(String input){
         System.out.println(separator);
         System.out.println(input);
+        System.out.println(separator);
+    }
+
+    public void printAddToList(String input){
+        System.out.println(separator);
+        System.out.println("Added: " + input);
+        System.out.println(separator);
+    }
+
+    public void printList(String[] input){
+        System.out.println(separator);
+        for (int i = 0; i < input.length; i+=1){
+            System.out.println(String.format("%d. ", i + 1) + input[i].trim());
+        }
         System.out.println(separator);
     }
 
